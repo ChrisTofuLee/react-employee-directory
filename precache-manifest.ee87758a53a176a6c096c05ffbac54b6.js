8bad0e8089a509608c8422909cefb56677fe2682
Selfay <chris.lee90@hotmail.co.uk>
2020-08-26 18:38:31 +0100

    Updates

---
+++ precache-manifest.ee87758a53a176a6c096c05ffbac54b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fd70e34dd05cc23961377af0a416eb",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "ea0d87093ca171475fe6",
+    "url": "/react-employee-directory/static/css/main.9865c041.chunk.css"
+  },
+  {
+    "revision": "2a25d7984ca53c5d03cd",
+    "url": "/react-employee-directory/static/js/2.57cbafba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.57cbafba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0d87093ca171475fe6",
+    "url": "/react-employee-directory/static/js/main.c088c225.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  }
+]);
